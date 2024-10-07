2bab1ddd74fd9901945865ac27456840db5ac23f
Hoggspjot <Andreyid67@gmail.com>
2024-10-07 19:54:46 +0300

    Correct first part

--- lesson106/Solution106.java
+++ lesson106/Solution106.java
@@ -1,38 +1,45 @@
+package com.javarush.task.pro.task09.task0908;
+
 
 /*
 Двоично-шестнадцатеричный конвертер
 */
 
+import java.util.regex.Pattern;
+
 public class Solution106 {
     private static final String HEX = "0123456789abcdef";
+
     public static void main(String[] args) {
-        String binaryNumber = "100111010000";
+        String binaryNumber = "100111010000111111";
         System.out.println("Двоичное число " + binaryNumber + " равно шестнадцатеричному числу " + toHex(binaryNumber));
-        String hexNumber = "9d0";
+        String hexNumber = "2743f";
         System.out.println("Шестнадцатеричное число " + hexNumber + " равно двоичному числу " + toBinary(hexNumber));
     }
 
     public static String toHex(String binaryNumber) {
-        String HEX = "0123456789abcdef";
-        String hexNum = "";
-        char chStr = '0';
-        if (binaryNumber == null || (binaryNumber.trim().isEmpty())) {
+        Pattern pattern = Pattern.compile("^[01]+$");
+        if (binaryNumber == null || (binaryNumber.trim().isEmpty()) || !pattern.matcher(binaryNumber).find()) {    //Проверка входящего значения на null b пустую строку и символы
             return "";
         }
+        String hexNum = "";
+        char chStr = '0';
         int dl = binaryNumber.length();
         String strFin = "";
-        if(dl % 4 != 0) {
-            while (dl % 4 != 0)
+        if (dl % 4 != 0) {
+            while (dl % 4 != 0) {
                 binaryNumber = "0" + binaryNumber;
+                dl = binaryNumber.length();
+            }
         }
-        for (int i = 0; i < dl; i+=4) {
-            String str = binaryNumber.substring(dl - (dl - i), dl - 8 + i);
-            strFin= str;
+        for (int i = 0, k = 4; k <= dl; i += 4, k +=4) {
+            String str = binaryNumber.substring(i, k);
+            strFin = str;
             int y = strFin.length();
             int x = 0;
             for (int j = 0; j < y; j++) {
-                String strNum = strFin.substring(strFin.length() - (j +1), strFin.length() - j);
-                int a = Integer.parseInt (strNum);
+                String strNum = strFin.substring(strFin.length() - (j + 1), strFin.length() - j);
+                int a = Integer.parseInt(strNum);
                 x = (int) (x + a * Math.pow(2, j));
             }
             chStr = HEX.charAt(x);
@@ -43,7 +50,8 @@ public class Solution106 {
     }
 
     public static String toBinary(String hexNumber) {
-        if (hexNumber == null || (hexNumber.trim().isEmpty())) {
+        Pattern pattern = Pattern.compile("[0-9a-f]+");
+        if (hexNumber == null || (hexNumber.trim().isEmpty()) || !pattern.matcher(hexNumber).matches()) {          //Проверка входящего значения на null b пустую строку и символы
             return "";
         }
         int x = 0;
@@ -55,16 +63,15 @@ public class Solution106 {
         }
         int y = 0;
         String decimal = "";
-        while (x != 0){
+        while (x != 0) {
             y = (x % 2) + y;
             decimal = y + decimal;
             y = 0;
-            x = x/2;
+            x = x / 2;
         }
         return decimal;
     }
-    }
-
+}
 
 
 
