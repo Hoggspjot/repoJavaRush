bc69ec38d9d858f7e4cea72b629848be67af795d
Hoggspjot <Andreyid67@gmail.com>
2024-10-07 18:32:37 +0300

    Added second part of solution

--- lesson106/Solution106.java
+++ lesson106/Solution106.java
@@ -1,13 +1,12 @@
 
-
 /*
 Двоично-шестнадцатеричный конвертер
 */
 
 public class Solution106 {
-
+    private static final String HEX = "0123456789abcdef";
     public static void main(String[] args) {
-        String binaryNumber = "100111010001";
+        String binaryNumber = "100111010000";
         System.out.println("Двоичное число " + binaryNumber + " равно шестнадцатеричному числу " + toHex(binaryNumber));
         String hexNumber = "9d0";
         System.out.println("Шестнадцатеричное число " + hexNumber + " равно двоичному числу " + toBinary(hexNumber));
@@ -35,19 +34,37 @@ public class Solution106 {
                 String strNum = strFin.substring(strFin.length() - (j +1), strFin.length() - j);
                 int a = Integer.parseInt (strNum);
                 x = (int) (x + a * Math.pow(2, j));
-                      }
+            }
             chStr = HEX.charAt(x);
             hexNum = hexNum + chStr;
 
-            }
-            return hexNum;
+        }
+        return hexNum;
     }
 
     public static String toBinary(String hexNumber) {
         if (hexNumber == null || (hexNumber.trim().isEmpty())) {
             return "";
         }
-        //напишите тут ваш код
-        return null;
+        int x = 0;
+        char str1 = '0';
+        for (int i = 0; i < hexNumber.length(); i++) {
+            str1 = hexNumber.charAt(i);
+            int index = HEX.indexOf(str1);
+            x = 16 * x + index;
+        }
+        int y = 0;
+        String decimal = "";
+        while (x != 0){
+            y = (x % 2) + y;
+            decimal = y + decimal;
+            y = 0;
+            x = x/2;
+        }
+        return decimal;
     }
-}
+    }
+
+
+
+
